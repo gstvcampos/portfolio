89c277f3cc6720cf6864fe6a09f7dd0dc97b6ee3
gustavo <gustavo.bmc76@gmail.com>
2023-12-04 14:02:13 -0300

    feat: icons

--- src/app/habilidades/page.tsx
+++ src/app/habilidades/page.tsx
@@ -1,30 +1,38 @@
-import CssIcon from '@/assets/icons/CssIcon'
+import {
+  CssIcon,
+  DjangoIcon,
+  HtmlIcon,
+  JavaScriptIcon,
+  NestIcon,
+  NextIcon,
+  NodeIcon,
+  PostgresIcon,
+  PrismaIcon,
+  PythonIcon,
+  ReactIcon,
+  StyledComponentsIcon,
+  TailwindIcon,
+  TypeScriptIcon,
+} from '@/components/Icons'
 
 export default function Habilidades() {
   return (
     <main className="p-4">
-      <div className="h-full w-full bg-content-2 p-4">
-        <div>
-          <CssIcon />
-        </div>
-        <h2 className="text-2xl">Ola, eu sou Gustavo</h2>
-        <p className="mb-6 text-color-3">desenvolvedor web fullstack</p>
-        <p className="mb-6">
-          sou um desenvolvedor fullstack apaixonado por tecnologia. Meu objetivo
-          é criar interfaces de usuário bonitas e funcionais. Estou sempre
-          aberto a novas oportunidades e desafios.
-        </p>
-        <h3 className="text-xl">Alguns fatos</h3>
-        <ul className="mb-6">
-          <li>Meu nome completo é Gustavo Campos Rodrigues</li>
-          <li>Eu nasci em 1995</li>
-          <li>Uma palavre para descrever minha personalidade: Introvertido</li>
-        </ul>
-        <h3 className="text-xl">Alguns curiosidades</h3>
-        <ul className="mb-6">
-          <li>sou vascaino</li>
-          <li>sou vascaino</li>
-        </ul>
+      <div className="bg-content-2">
+        <CssIcon />
+        <HtmlIcon />
+        <DjangoIcon />
+        <JavaScriptIcon />
+        <NestIcon />
+        <NextIcon />
+        <NodeIcon />
+        <PostgresIcon />
+        <PrismaIcon />
+        <PythonIcon />
+        <ReactIcon />
+        <StyledComponentsIcon />
+        <TailwindIcon />
+        <TypeScriptIcon />
       </div>
     </main>
   )

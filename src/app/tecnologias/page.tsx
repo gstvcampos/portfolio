3e0127685c360d1d53b19f791d8342274d275a2d
gustavo <gustavo.bmc76@gmail.com>
2023-12-07 17:26:16 -0300

    feat: tecnologies icons

--- src/app/tecnologias/page.tsx
+++ src/app/tecnologias/page.tsx
@@ -9,14 +9,21 @@ export default function Tecnologias() {
       <section className="flex h-[calc(100%-48px)] flex-col gap-4 overflow-auto p-4 md:flex-row">
         <ul className="grid w-full grid-cols-3 gap-4 bg-content-2 p-4 md:grid-cols-4 lg:grid-cols-5">
           {technologies.map((tech, index) => (
-            <li
-              key={index}
-              className="flex flex-col items-center justify-between"
-            >
-              <Link target="_blank" href={tech.documentationLink}>
-                <span className="flex h-20 w-20">{tech.icon}</span>
+            <li key={index}>
+              <Link
+                target="_blank"
+                href={tech.documentationLink}
+                className="group relative box-border flex w-full flex-col items-center text-center"
+              >
+                <span className="mb-1 rounded-3xl bg-color-3 p-6 transition duration-500 ease-in-out group-hover:rounded-full group-hover:bg-color-2">
+                  <span className="block h-12 w-12 transition duration-1000 ease-in-out group-hover:scale-125">
+                    {tech.icon}
+                  </span>
+                </span>
+                <span className="font-segoe-bold group-hover:text-color-2">
+                  {tech.name}
+                </span>
               </Link>
-              <span className="font-segoe-bold text-center">{tech.name}</span>
             </li>
           ))}
         </ul>

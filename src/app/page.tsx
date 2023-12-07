75ec60e7f5929c3a39ac8bd63e69aff41bba4a91
gustavo <gustavo.bmc76@gmail.com>
2023-12-07 16:48:59 -0300

    feat: icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
+import { MailIcon, ResumeIcon } from '@/components/Icons'
 import Image from 'next/image'
-import { CiMail, CiViewList } from 'react-icons/ci'
+import Link from 'next/link'
 
 export default async function Home() {
   return (
@@ -18,38 +19,52 @@ export default async function Home() {
           />
 
           <div className="mt-4 flex flex-wrap justify-center gap-4 md:flex-col">
-            <button className="w-44 border-b-4 border-color-3 bg-content-2 md:w-full">
-              <span className="flex items-center gap-4 px-4 py-3">
-                <CiViewList size={30} />
-                <span>Currículo</span>
+            <Link
+              href={''}
+              className="min-w-[150px] border-b-4 border-color-3 bg-content-2 md:w-full"
+            >
+              <span className="flex items-center">
+                <span className="border-r-[1px] border-color-3 px-3 py-1">
+                  <span className="flex w-6">
+                    <ResumeIcon />
+                  </span>
+                </span>
+                <span className="px-4 py-3">Currículo</span>
               </span>
-            </button>
-            <button className="w-44 border-b-4 border-color-3 bg-content-2 md:w-full">
-              <span className="flex items-center gap-4 px-4 py-3">
-                <CiMail size={30} />
-                <span>Email</span>
+            </Link>
+            <Link
+              href={''}
+              className="min-w-[150px] border-b-4 border-color-3 bg-content-2 md:w-full"
+            >
+              <span className="flex items-center">
+                <span className="border-r-[1px] border-color-3 px-3 py-1">
+                  <span className="flex w-6">
+                    <MailIcon />
+                  </span>
+                </span>
+                <span className="px-4 py-3">Email</span>
               </span>
-            </button>
+            </Link>
           </div>
         </div>
 
         <div className="h-full w-full bg-content-2 p-4">
-          <h2 className="text-2xl">Ola, eu sou Gustavo</h2>
-          <p className="mb-6 text-color-3">desenvolvedor web fullstack</p>
-          <p className="mb-6">
+          <h2 className="font-segoe-bold text-2xl">Ola, eu sou Gustavo</h2>
+          <p className="text-color-3">desenvolvedor web fullstack</p>
+          <p className="m-2">
             sou um desenvolvedor fullstack apaixonado por tecnologia. Meu
             objetivo é criar interfaces de usuário bonitas e funcionais.
           </p>
-          <h3 className="text-xl">Alguns fatos</h3>
-          <ul className="mb-6">
+          <h3 className="font-segoe-bold">Alguns fatos</h3>
+          <ul className="m-2">
             <li>Meu nome completo é Gustavo Campos Rodrigues</li>
             <li>Eu nasci em 1995</li>
             <li>
               Uma palavre para descrever minha personalidade: Introvertido
             </li>
           </ul>
-          <h3 className="text-xl">Alguns curiosidades</h3>
-          <ul className="mb-6">
+          <h3 className="font-segoe-bold">Alguns curiosidades</h3>
+          <ul className="m-2">
             <li>sou vascaino</li>
             <li>sou vascaino</li>
           </ul>

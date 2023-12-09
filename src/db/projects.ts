b4f5d8ed496f67c352b2a5429ea8e7c7e23821c2
gustavo <gustavo.bmc76@gmail.com>
2023-12-09 11:40:26 -0300

    feat: descriptions

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     name: 'Kenzie Hub',
     description:
-      'Aqui, você encontrará recursos robustos de cadastro e login, intuitivas para simplificar sua jornada na plataforma.',
+      'Este projeto é uma aplicação web robusta, desenvolvida com TypeScript, React e Styled Components. A aplicação integra-se a uma API para fornecer funcionalidades de autenticação, permitindo aos usuários se cadastrarem e fazerem login. Para garantir a precisão dos dados, todos os formulários de preenchimento utilizam a biblioteca Zod para validação de campos. Além disso, a aplicação utiliza o React-Toastify para exibir as respostas da API de uma maneira amigável ao usuário. Após o login bem-sucedido, um token de autenticação é armazenado no localStorage, permitindo a persistência da sessão do usuário. Na dashboard, implementei uma proteção de rota para garantir que apenas usuários autenticados tenham acesso, melhorando a segurança e a experiência do usuário.',
     deploy: 'https://react-entrega-kenzie-hub-gstvcampos.vercel.app/',
     main: 'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-kenzie-hub-gstvcampos/tree/main',
   },
@@ -11,7 +11,7 @@ export const projects = [
     id: 2,
     name: 'Burguer',
     description:
-      'Navegue pelo cardápio virtual repleto de hambúrgueres suculentos, para te deixar com água na boca. Adicione seus favoritos ao carrinho de compras, desfrute de uma experiência visualmente atraente e mergulhe na simplicidade de pedir hambúrgueres deliciosos online.',
+      'Este projeto é uma loja virtual de hambúrgueres dinâmica e interativa, desenvolvida com JavaScript, React e Styled Components. A aplicação se integra a uma API para listar uma variedade de hambúrgueres disponíveis para compra. Um dos recursos notáveis é um campo de busca em tempo real que filtra os itens listados com base no nome ou categoria inseridos, proporcionando uma experiência de usuário mais personalizada e eficiente. Além disso, a aplicação possui um carrinho de compras interativo implementado como um modal. Este modal pode ser fechado de várias maneiras - pressionando a tecla "Esc", clicando fora do modal ou no botão de fechar - oferecendo ao usuário flexibilidade no controle da interface. O carrinho de compras exibe todos os itens selecionados junto com a soma total, permitindo que os usuários revisem e modifiquem facilmente suas seleções antes de finalizar a compra.',
     deploy:
       'https://react-entrega-s3-template-hamburgueria-gstvcampos.vercel.app/',
     main: 'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s3-template-hamburgueria-gstvcampos/tree/main',
@@ -20,7 +20,7 @@ export const projects = [
     id: 3,
     name: 'Nu Kenzie',
     description:
-      'Seu aliado no controle financeiro pessoal! Aqui, você encontrará uma aplicação intuitiva para gerenciar suas entradas e saídas financeiras. A experiência é focada na simplicidade: preencha o formulário com suas transações, acompanhe o saldo total e desfrute de uma interface estilizada para tornar o controle financeiro eficiente e agradável.',
+      'Este projeto é um aplicativo de controle financeiro intuitivo e fácil de usar, desenvolvido com JavaScript, React e Styled Components. A aplicação permite aos usuários lançar todas as suas receitas e despesas por meio de um formulário simplificado. Todas as transações são então listadas e o saldo total é calculado e exibido, proporcionando uma visão clara da situação financeira do usuário. A interface do usuário, projetada com foco na simplicidade e na eficiência, torna o gerenciamento financeiro não apenas uma tarefa fácil, mas também uma experiência agradável.',
     deploy:
       'https://react-entrega-s1-template-nu-kenzie-gstvcampos.vercel.app/',
     main: 'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-template-nu-kenzie-gstvcampos/tree/master',
@@ -29,30 +29,22 @@ export const projects = [
     id: 4,
     name: 'Contatos',
     description:
-      'Aqui, você encontrará uma aplicação intuitiva para gerenciar clientes e seus contatos vinculados.',
+      'Este projeto é uma aplicação full stack robusta para gerenciamento de clientes e seus contatos. O back-end foi construído com Nest.js e Prisma, criando um relacionamento de um para muitos entre clientes e contatos em duas tabelas, permitindo que um único cliente tenha vários contatos. No front-end, a aplicação foi desenvolvida com Next.js e Tailwind CSS, proporcionando uma interface de usuário moderna e responsiva. Para otimizar o desempenho, todos os botões foram configurados para renderizar no lado do servidor. A aplicação lista todos os clientes cadastrados e oferece funcionalidades para adicionar, editar e excluir clientes e contatos. Este projeto demonstra minha capacidade de criar aplicações full stack eficientes e fáceis de usar.',
     deploy: 'https://fullstack-customer-contact-gstvcampos.vercel.app/',
     main: 'https://github.com/gstvcampos/fullstack-customer-contact-gstvcampos/tree/main',
   },
   {
     id: 5,
-    name: 'MKS',
-    description:
-      'A aplicação consiste em uma loja online que consome uma API REST de produtos para exibir uma lista de itens disponíveis para compra. Esta aplicação foi desenvolvida como parte de um desafio técnico,  seguindo as diretrizes e requisitos fornecidos.',
-    deploy: 'https://mks-frontend-challenge-sigma.vercel.app/',
-    main: 'https://github.com/gstvcampos/mks-frontend-challenge/tree/main',
-  },
-  {
-    id: 6,
     name: 'Kanvas',
     description:
-      'A API Kanvas oferece uma solução robusta para gerenciamento eficiente de cursos e aulas',
+      'Este projeto é um aplicativo de gerenciamento de cursos e aulas. A aplicação foi desenvolvida utilizando a linguagem Python, com o auxílio do framework Django e do Django Rest Framework e postegres para o armazenamento de dados. A estrutura da aplicação inclui diversos relacionamentos entre as entidades Account, Course e Content, permitindo uma gestão eficiente dos cursos e dos alunos. Além disso, a aplicação conta com um sistema de views concretas e Model Serializer do DRF, garantindo a consistência e a qualidade do código.',
     main: 'https://github.com/gstvcampos/m5-projeto-final-kanvas-gstvcampos/tree/main',
   },
   {
-    id: 7,
+    id: 6,
     name: 'KiMoveis',
     description:
-      'A API KImóveis oferece funcionalidades para o gerenciamento de uma imobiliária. Ela permite cadastrar imóveis, usuários interessados na aquisição de propriedades, agendar visitas e consultar horários disponíveis para visitas aos imóveis.',
+      'Este projeto é uma aplicação voltada para o mercado imobiliário, foi desenvolvida utilizando TypeScript, Zod, TypeORM e PostgreSQL como o banco de dado. A aplicação conta com uma estrutura de cinco tabelas, responsáveis por armazenar os dados dos usuários, as categorias de imóveis, os imóveis em si e os agendamentos de visitas. A aplicação permite que os usuários agendem visitas às propriedades de seu interesse, oferecendo uma visão clara e atualizada dos horários disponíveis.',
     main: 'https://github.com/Kenzie-Academy-Brasil-Developers/m4-kimoveis-gstvcampos',
   },
 ]

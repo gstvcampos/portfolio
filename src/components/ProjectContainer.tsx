c3d084670bf1390da53d842e47f9c99601344327
gustavo <gustavo.bmc76@gmail.com>
2023-12-09 11:56:28 -0300

    feat: descriptions

--- src/components/ProjectContainer.tsx
+++ src/components/ProjectContainer.tsx
@@ -6,16 +6,16 @@ export default function ProjectContainer({ id }: { id: number }) {
     <div className="w-full bg-content-2 ">
       {projects.map((project) =>
         project.id === id ? (
-          <div key={project.id} className="flex flex-wrap p-4 md:flex-nowrap">
-            <div className="m-4 flex max-w-full flex-col gap-6">
-              <h2 className="font-segoe-bold text-2xl">{project.name}</h2>
-              <p>{project.description}</p>
-              <div className="flex w-fit gap-5">
-                {project.deploy ? (
-                  <LinkSmall href={project.deploy} innerText="Deploy" />
-                ) : null}
-                <LinkSmall href={project.main} innerText="GitHub" />
-              </div>
+          <div key={project.id} className="m-4 flex flex-col gap-2 p-4">
+            <h2 className="mb-2 font-segoe-bold text-2xl text-text-2">
+              {project.name}
+            </h2>
+            <p>{project.description}</p>
+            <div className="flex w-fit gap-5">
+              {project.deploy ? (
+                <LinkSmall href={project.deploy} innerText="Deploy" />
+              ) : null}
+              <LinkSmall href={project.main} innerText="GitHub" />
             </div>
           </div>
         ) : null,

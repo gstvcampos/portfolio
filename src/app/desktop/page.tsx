f3173938e9185caad9b585b7f1d5f505740fe986
gustavo <gustavo.bmc76@gmail.com>
2023-12-12 12:09:33 -0300

    fix: window bar

---
+++ src/app/desktop/page.tsx
@@ -0,0 +1,3 @@
+export default async function Desktop() {
+  return <span />
+}
--- src/components/WindowBar.tsx
+++ src/components/WindowBar.tsx
@@ -1,20 +1,42 @@
+'use client'
+
 import { CloseIcon, MaximizeIcon, MinimizeIcon } from '@/db/Icons'
+import Link from 'next/link'
 
 export default function WindowBar({ innerText }: { innerText: string }) {
+  interface FullscreenElement extends HTMLElement {
+    mozRequestFullScreen?: () => Promise<void>
+    webkitRequestFullscreen?: () => Promise<void>
+    msRequestFullscreen?: () => Promise<void>
+  }
+
+  const goFullScreen = () => {
+    const docEl = document.documentElement as FullscreenElement
+    const requestFullScreen =
+      docEl.requestFullscreen ||
+      docEl.mozRequestFullScreen ||
+      docEl.webkitRequestFullscreen ||
+      docEl.msRequestFullscreen
+
+    if (requestFullScreen) {
+      requestFullScreen.call(docEl)
+    }
+  }
+
   return (
     <div className="flex h-10 items-center justify-between border-b-[1px]">
       <span></span>
       <h1>{innerText}</h1>
       <div className="flex h-full gap-3 p-2">
-        <button className="rounded-full bg-focus p-1">
+        <Link className="rounded-full bg-focus p-1" href={'/desktop'}>
           <MinimizeIcon />
-        </button>
-        <button className="rounded-full bg-focus p-1">
+        </Link>
+        <button className="rounded-full bg-focus p-1" onClick={goFullScreen}>
           <MaximizeIcon />
         </button>
-        <button className="rounded-full bg-focus p-1">
+        <Link className="rounded-full bg-focus p-1" href={'/desktop'}>
           <CloseIcon />
-        </button>
+        </Link>
       </div>
     </div>
   )

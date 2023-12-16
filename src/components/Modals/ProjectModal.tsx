33e6e444ed4c9cc4e5e2d2873ea983e257665ec9
gustavo <gustavo.bmc76@gmail.com>
2023-12-16 17:39:06 -0300

    fix: modal

--- src/components/Modals/ProjectModal.tsx
+++ src/components/Modals/ProjectModal.tsx
@@ -46,42 +46,44 @@ export default function ProjectModal({
             </button>
           </div>
         </div>
-        <div className="flex max-h-[calc(100vh-220px)] flex-col gap-8 overflow-auto bg-content-2 p-4">
-          <div className="flex flex-col gap-3">
-            {project.description.split('\n').map((paragraph, index) => {
-              return <p key={index}># ~ {paragraph}</p>
-            })}
-          </div>
-          <div className="flex flex-wrap gap-2">
-            {project.technologies.map((tech, index) => (
-              <p className="rounded-xl border-2 px-2 text-sm" key={index}>
-                {tech}
-              </p>
-            ))}
-          </div>
-          <div className="flex flex-wrap gap-4">
-            {project.deploy && (
+        <div className="max-h-[calc(100vh-220px)] gap-8 overflow-auto bg-content-2">
+          <div className="flex flex-col gap-8 p-4">
+            <div className="flex flex-col gap-3">
+              {project.description.split('\n').map((paragraph, index) => {
+                return <p key={index}># ~ {paragraph}</p>
+              })}
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {project.technologies.map((tech, index) => (
+                <p className="rounded-xl border-2 px-2 text-sm" key={index}>
+                  {tech}
+                </p>
+              ))}
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {project.deploy && (
+                <Link
+                  target="_blank"
+                  href={project.deploy}
+                  className="inline-block h-8 w-24 rounded-md bg-txt-1 text-content-2"
+                >
+                  <span className="flex h-full items-center gap-2 px-2">
+                    <LiveIcon className="h-4 w-4" />
+                    live
+                  </span>
+                </Link>
+              )}
               <Link
                 target="_blank"
-                href={project.deploy}
+                href={project.main}
                 className="inline-block h-8 w-24 rounded-md bg-txt-1 text-content-2"
               >
                 <span className="flex h-full items-center gap-2 px-2">
-                  <LiveIcon className="h-4 w-4" />
-                  live
+                  <BranchIcon className="h-4 w-4" />
+                  branch
                 </span>
               </Link>
-            )}
-            <Link
-              target="_blank"
-              href={project.main}
-              className="inline-block h-8 w-24 rounded-md bg-txt-1 text-content-2"
-            >
-              <span className="flex h-full items-center gap-2 px-2">
-                <BranchIcon className="h-4 w-4" />
-                branch
-              </span>
-            </Link>
+            </div>
           </div>
         </div>
       </div>

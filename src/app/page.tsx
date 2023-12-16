021896a3ad8fd2d2eb2ffbe792535ba15c2812e3
gustavo <gustavo.bmc76@gmail.com>
2023-12-16 18:23:31 -0300

    fix: windowsbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       className="flex max-h-full min-h-full min-w-full max-w-full flex-col overflow-hidden md:min-h-0 md:rounded-lg lg:max-h-[calc(100%-16px)] lg:min-w-[900px] lg:max-w-[900px]"
     >
       <WindowBar innerText="Perfil" elementId="home" />
-      <section className="flex-1 overflow-auto bg-content-2">
+      <section className="h-[calc(100%-40px)] flex-1 overflow-auto bg-content-2">
         <div className="flex h-full min-w-0 flex-col md:flex-row">
           <div className="min-w-0 flex-shrink-0 bg-content-1 p-4 text-center">
             <Image

3ca7b388cd6afde95896424b3a3c6eebe631ccc2
gustavo <gustavo.bmc76@gmail.com>
2023-12-15 21:02:01 -0300

    fix: prettier

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 function CustomImage({ src, alt }: { src: string; alt: string }) {
   return <Image priority={true} src={src} width={48} height={48} alt={alt} />
